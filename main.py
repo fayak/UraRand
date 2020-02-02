1eccf140556ea15afb67b332e228c2eef4166448
Zarak <cyril@fayak.com>
2020-02-02 17:20:06 +0000

    collect: add collect method
    Signed-off-by: Zarak <cyril@fayak.com>

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import urarand.collect
 
 def main():
     config = urarand.config.get_config()
-    urarand.collect.collect()
+    urarand.collect.collect(lambda x : print('*', end='', flush=True))
 
 if __name__ == "__main__":
     main()
--- urarand/collect.py
+++ urarand/collect.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python3
 
+import sys
+import signal
 import time
+import smbus
+import RPi.GPIO as GPIO
 
-def collect():
-    print("Starting collection")
-    while True:
+I2C_ADDR = 0x19
+GPIO_PIN = 7
+
+loop = True
+
+def _init_counter(callback):
+    bus = smbus.SMBus(1)
+    bus.write_byte(I2C_ADDR, 0x71)
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(GPIO_PIN, GPIO.IN)
+
+    GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING)
+    GPIO.add_event_callback(GPIO_PIN, callback)
+
+def _stop_counter():
+    bus = smbus.SMBus(1)
+    bus.write_byte(I2C_ADDR, 0)
+
+def _signal_handler(sig, frame):
+    global loop
+    loop = False
+
+def collect(callback):
+    global loop
+    _init_counter(callback)
+
+    signal.signal(signal.SIGINT, _signal_handler)
+    loop = True
+    while loop:
         time.sleep(0.1)
-        # Do stuff
+    _stop_counter()
