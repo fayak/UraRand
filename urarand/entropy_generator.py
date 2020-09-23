a65b0cc8eef1144182c682888441bef11bf41a4e
Zarak <cyril@fayak.com>
2020-09-23 17:14:49 +0200

    rework tick function, add stats (WIP)
    Signed-off-by: Zarak <cyril@fayak.com>

--- urarand/entropy_generator.py
+++ urarand/entropy_generator.py
@@ -11,6 +11,29 @@ class EntropyGeneratorModel():
     def dump_entropy(self):
         raise NotImplementedError(f"{self.__qualname__} should be implemented")
 
+def get_tick_function():
+    """
+        This function returns the function to get the current tick, depending
+        on the OS/CPU/whatever precision
+    """
+
+    f = lambda divisor : lambda : time.perf_counter_ns() // divisor
+
+    for divisor in range(9):
+        for j in range(100):
+            ts = f(divisor)()
+            if ts % 10 != 0:
+                return stats(f(divisor))
+
+    raise Exception("time.perf_counter_ns() seems buggy ?")
+
+def stats():
+    def decorator(func):
+        ts = func()
+        print(ts % 10)
+        return ts
+    return decorator
+
 @dataclass
 class EntropyGenerator(EntropyGeneratorModel):
     entropy_path: str = "./" + str(__qualname__) + ".bin"
@@ -27,11 +50,12 @@ class EntropyGenerator(EntropyGeneratorModel):
 
     t0: int = 0
 
-    get_tick: Callable[[], int] = lambda : time.perf_counter_ns() // 1000
+    get_tick: Callable[[], int] = get_tick_function()
 
     def _delta_to_bits(self, delta):
         return str(delta % 2)
 
+    @stats
     def tick(self, *args, **kwargs):
         if self.counter == 0:
             self.t0 = self.get_tick()
@@ -53,7 +77,7 @@ class EntropyGenerator(EntropyGeneratorModel):
             self.dump_entropy()
             self.entropy = b""
             self.counter = 0
-    
+
     def dump_entropy(self):
         with open(self.entropy_path, 'ab+') as f:
             f.write(self.entropy)
